63df27daa16d381b3ad2cb5314a1db993983c762
grigdim <d.g.grigoriadis@gmail.com>
2025-12-11 18:06:46 +0200

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Dim Grigoriadis on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
